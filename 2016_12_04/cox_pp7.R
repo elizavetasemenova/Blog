a7186495f48c553b9cadf1def0cddc6d433eb8ed
Elizaveta Semenova <elizaveta.p.semenova@gmail.com>
2017-02-23 10:09:25 +0100

    Create cox_pp7.R

---
+++ 2016_12_04/cox_pp7.R
@@ -0,0 +1,10 @@
+> print(fit.r)
+Inference for Stan model: cox.
+3 chains, each with iter=10000; warmup=3000; thin=1; 
+post-warmup draws per chain=7000, total post-warmup draws=21000.
+
+             mean se_mean    sd      2.5%       25%       50%       75%     97.5% n_eff Rhat
+beta0        2.56    0.00  0.05      2.47      2.53      2.56      2.59      2.65   765    1
+beta1        0.20    0.00  0.01      0.17      0.19      0.20      0.21      0.23   670    1
+sigma_r      1.29    0.00  0.03      1.22      1.26      1.29      1.31      1.35 21000    1
+lp__    164065.17    0.29 22.89 164018.73 164050.14 164065.79 164080.74 164108.63  6316    1
